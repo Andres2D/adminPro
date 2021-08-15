eb2c3efffb5dae2f79c4b18ee18740cd0470af35
Andres2D <andres2d1997.1206@gmail.com>
2021-08-14 20:27:27 -0500

    Adding dinamyc sidebar menu

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    {
+      title: 'Dashboard',
+      icon: 'mdi mdi-gauge',
+      subMenu: [
+        {
+          title: 'Main',
+          url: '/'
+        },
+        {
+          title: 'Progressbarr',
+          url: 'progress'
+        },
+        {
+          title: 'Graphs',
+          url: 'graph1'
+        }
+      ]
+    }
+  ];
+
+  constructor() { }
+
+
+}
